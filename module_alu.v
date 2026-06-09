10d8cd31140b6c9391b6bc11ab5b1e3b9eb1611b
lecaio08 <clmichalewicz@gmail.com>
2026-06-09 15:37:50 -0300

    Add immediate value handling to ALU module

--- module_alu.v
+++ module_alu.v
@@ -1,8 +1,10 @@
 module module_alu (
-    input             [2:0]  opcode, // codigo da instrução a ser decodificado pela unit control
-    input signed      [15:0] opA,    // operando A
-    input signed      [15:0] opB,    // operando B
-    output reg signed [15:0] result  // ?
+    input [2:0] opcode,
+    input signed [15:0] opA,
+    input signed [15:0] opB,
+    input signed [15:0] imm_ext, // Entrada do imediato vindo da CU
+    input use_imm,               // Flag se deve usar o imediato
+    output reg signed [15:0] result
 );
     // opcodes de acordo com as especificações
     parameter LOAD    = 3'b000; // LOAD x1, a
@@ -14,16 +16,16 @@ module module_alu (
     parameter CLEAR   = 3'b110; // zera os registradores
     parameter DISPLAY = 3'b111; // DISPLAY x0
     // parte combinacional: definição dos outputs de acordo com a instrução
-    always @(*) begin
+always @(*) begin
         case (opcode)
-            LOAD:    result = opB;       
-            ADD:     result = opA + opB;
-            ADDI:    result = opA + opB;
-            SUB:     result = opA - opB;
-            SUBI:    result = opA - opB;
-            MUL:     result = opA * opB;
-            CLEAR:   result = 16'd0; 
-            DISPLAY: result = opA;
+            3'b000:  result = imm_ext; // LOAD joga o imediato direto no registrador
+            3'b001:  result = opA + opB;
+            3'b010:  result = opA + imm_ext; // Usa o Imediato
+            3'b011:  result = opA - opB;
+            3'b100:  result = opA - imm_ext; // Usa o Imediato
+            3'b101:  result = opA * opB;
+            3'b110:  result = 16'd0; 
+            3'b111:  result = opA;
             default: result = 16'd0;
         endcase
     end
